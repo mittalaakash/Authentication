d4014f4e163270322b636ad781abfac33eba8438
mittalaakash <mit.ash.619@gmail.com>
2021-09-09 18:55:44 +0530

    live database

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-// const DB = process.env.DATABASE.replace(
-//   '<PASSWORD>',
-//   process.env.DATABASE_PASSWORD,
-// );
-const DB = process.env.DATABASE_LOCAL;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+// const DB = process.env.DATABASE_LOCAL;
 mongoose.connect(DB, () => {
   console.log('connected to database');
 });
